823555f3d3d10c8364f926feca98a464e5842f15
Adam Myślicki <revixxdm@gmail.com>
2022-06-13 10:57:30 +0200

    Updates

---
+++ precache-manifest.bee454081345539966ea4734c280809a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8ef47721ecb29571e2b",
+    "url": "/Portfolio/static/css/main.835049fa.chunk.css"
+  },
+  {
+    "revision": "a8ef47721ecb29571e2b",
+    "url": "/Portfolio/static/js/main.a8ef4772.chunk.js"
+  },
+  {
+    "revision": "6d6233583aeda429c9e5",
+    "url": "/Portfolio/static/js/1.6d623358.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "01a99ffbf282b1ce2aac99b636287185",
+    "url": "/Portfolio/static/media/Edie-Homepage.01a99ffb.jpg"
+  },
+  {
+    "revision": "65ddb986e4f5458d599683fbd98d636f",
+    "url": "/Portfolio/static/media/Recipe-Page.65ddb986.jpg"
+  },
+  {
+    "revision": "388aa91e9efdc728e60825ad39fe06ac",
+    "url": "/Portfolio/static/media/to-do-list.388aa91e.jpg"
+  },
+  {
+    "revision": "2fd9d262f3acb440c3fba4e2a9558db0",
+    "url": "/Portfolio/static/media/windBnB.2fd9d262.jpg"
+  },
+  {
+    "revision": "2fccf03507cff86cef38f62947b12786",
+    "url": "/Portfolio/index.html"
+  }
+];
