2d9b5ca9c6f28165c51c5990117ffa3c92000165
Adam Myślicki <revixxdm@gmail.com>
2022-08-23 17:20:36 +0200

    adding use state in Project component to wait for data other

--- src/App.js
+++ src/App.js
@@ -1,67 +1,57 @@
-import React from "react";
+import React from 'react'
 
-//import data.js for infos 
-import data from "./data/data"
+//import data.js for infos
+import data from './data/data'
 //Components import
-import BasicInfo from "./components/BasicInfo"
-import Skills from "./components/Skills"
-import Projects from "./components/Projects"
-import Hobbies from "./components/Hobbies"
-import Footer from "./components/Footer"
+import BasicInfo from './components/BasicInfo'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Hobbies from './components/Hobbies'
+import Footer from './components/Footer'
 
-import "./styles/app.css"
+import './styles/app.css'
 
 //styles for darkMode
-import { ThemeProvider } from "styled-components";
-import {lightTheme, darkTheme, GlobalStyles} from "./components/darkmode/themes"
-
-
+import { ThemeProvider } from 'styled-components'
+import {
+    lightTheme,
+    darkTheme,
+    GlobalStyles,
+} from './components/darkmode/themes'
 
 function App() {
+    //darkMode functions
+    const [theme, setTheme] = React.useState('light')
 
-  //darkMode functions 
-  const[theme, setTheme] = React.useState("light")
+    function changeTheme() {
+        theme === 'light' ? setTheme('dark') : setTheme('light')
+    }
 
-  function changeTheme(){
-    theme === "light" ? setTheme("dark") : setTheme("light")
-  }
-
-
-
-  return (
-
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <GlobalStyles />
-      <div className="accessibilityDiv">
-
-        <button onClick={changeTheme}>Theme</button>
-        {/* <button >Language</button> */}
-        {/* <br></br>
+    return (
+        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+            <GlobalStyles />
+            <div className="accessibilityDiv">
+                <button onClick={changeTheme}>Theme</button>
+                {/* <button >Language</button> */}
+                {/* <br></br>
         <button> test/></button> */}
+            </div>
+            <div className="container">
+                <div className="gridContainer">
+                    <BasicInfo />
 
-      </div>
-      <div className="container">
-      
-        <div className="gridContainer">
+                    <Skills name="skills" array={data.skillsTech} />
+                    <Skills name="front-end" array={data.skillsFront} />
 
-        <BasicInfo/>
+                    <Projects />
 
-        <Skills name = "skills" array = {data.skillsTech} />
-        <Skills name = "front-end" array = {data.skillsFront} />
+                    <Hobbies array={data.hobbies} />
+                </div>
 
-        <Projects />
-
-        <Hobbies array = {data.hobbies} />
-
-      </div>
-
-      <Footer />
-        
-      </div>
-
-    </ThemeProvider>
-
-  )
+                <Footer />
+            </div>
+        </ThemeProvider>
+    )
 }
 
-export default App;
+export default App
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React from 'react'
 
-import ProjectsElement from "./ProjectsElement";
+import ProjectsElement from './ProjectsElement'
 
-import "../styles/projects.css"
+import '../styles/projects.css'
 
-function Projects(){
-
+function Projects() {
     const [pinnedRepos, setPinnedRepos] = React.useState([])
+    const [elementIsLoading, setELementIsLoading] = React.useState(false)
 
     //fetching data from api
-    async function fetchPinnedFromProfile(){
-        const response = await fetch(`https://gh-pinned-repos.egoist.sh/?username=revixxd`);
-        const data = await response.json();
+    async function fetchPinnedFromProfile() {
+        const response = await fetch(
+            `https://gh-pinned-repos.egoist.sh/?username=revixxd`
+        )
+        const data = await response.json()
         //data is imported repos
-        return setPinnedRepos(data);
+        setELementIsLoading(false)
+        return setPinnedRepos(data)
     }
 
     // on first load set imported data from api to pinnedRepos
@@ -22,28 +25,27 @@ function Projects(){
     }, [])
 
     //filtering repos to not show Portfolio repo and c exercises
-    const fillterData = pinnedRepos.filter(function(element){
-        return !(element.repo === "Portfolio" || element.repo === "C-C--exercises")
-    })
-
-    const allPinedProjects = fillterData.map((element, i) =>{
-        return(
-            <ProjectsElement key={i} array={element}/>
+    const fillterData = pinnedRepos.filter(function(element) {
+        return !(
+            element.repo === 'Portfolio' || element.repo === 'C-C--exercises'
         )
     })
 
+    const allPinedProjects = fillterData.map((element, i) => {
+        return <ProjectsElement key={i} array={element} />
+    })
 
-    return(
+    return (
         <div className="projectsDiv">
-            
-            <div className = "projectsDiv--titileDiv elementStyle">
-                <h3>Projects ({Object.keys(fillterData).length})</h3>    
+            <div className="projectsDiv--titileDiv elementStyle">
+                <h3>Projects ({Object.keys(fillterData).length})</h3>
             </div>
 
-            <div className = "projectsDiv--elements">
-                {allPinedProjects}
-            </div>
-            
+            {elementIsLoading ? (
+                <div className="projectsDiv--elements">{allPinedProjects}</div>
+            ) : (
+                <h1>LOADING</h1>
+            )}
         </div>
     )
 }
