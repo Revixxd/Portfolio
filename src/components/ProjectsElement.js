e40a7f8eb809376fddd39e799b22be071db5859d
Adam Myślicki <revixxdm@gmail.com>
2022-09-06 20:04:58 +0200

    adding useLocalStorage custom hook

--- src/components/ProjectsElement.js
+++ src/components/ProjectsElement.js
@@ -1,43 +1,40 @@
-import React from "react"
+import React from 'react'
 
-import "../styles/projectElements.css"
+import '../styles/projectElements.css'
 
-function ProjectsElement(props){
-
+function ProjectsElement(props) {
     const currentImg = `${props.array.repo}.jpg`
-    return(
+    return (
         <div className="projectElementsDiv elementStyle">
-
             <div className="projectElementsDiv--imgDiv">
-
-                <img className = "projectElementsDiv--img" alt="projectImg" 
-                src={currentImg && require(`../data/images/${currentImg}`)}>
-                    
-                </img>
-
+                <img
+                    className="projectElementsDiv--img"
+                    alt="projectImg"
+                    src={
+                        currentImg &&
+                        require(`../resources/project-images/${currentImg}`)
+                    }
+                ></img>
             </div>
 
             <div className="projectElementsDiv--info">
-                
-                {props.array.language === "HTML" && 
+                {props.array.language === 'HTML' && (
                     <div className="projectElementsDiv__info--hastag">
                         <h6>#HTML</h6>
                         <h6>#CSS</h6>
                         <h6>#WebResponsive</h6>
                     </div>
-                }
+                )}
 
-                {props.array.language === "JavaScript" && 
+                {props.array.language === 'JavaScript' && (
                     <div className="projectElementsDiv__info--hastag">
                         <h6>#React</h6>
                         <h6>#StyledComponents</h6>
                         <h6>#WebResponsive</h6>
                     </div>
-                }
-
+                )}
 
                 <div className="projectElementsDiv__info--mainDescription">
-
                     <div className="projectElementsDiv__info__mainDescription--projectName">
                         <h2>{props.array.repo}</h2>
                     </div>
@@ -45,22 +42,29 @@ function ProjectsElement(props){
                     <div className="projectElementsDiv__info__mainDescription--description">
                         <p>{props.array.description} </p>
                     </div>
-
                 </div>
 
-                
-
-                <div className="projectElementsDiv__info--buttonsDiv" >
-                    <a className="projectElementsDiv__info__buttonsDiv-aElement" target="_blank" rel="noopener noreferrer" href={props.array.website}>Demo</a>
-                    <a className="projectElementsDiv__info__buttonsDiv-aElement" target="_blank" rel="noopener noreferrer" href={props.array.link}>Code</a>
+                <div className="projectElementsDiv__info--buttonsDiv">
+                    <a
+                        className="projectElementsDiv__info__buttonsDiv-aElement"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={props.array.website}
+                    >
+                        Demo
+                    </a>
+                    <a
+                        className="projectElementsDiv__info__buttonsDiv-aElement"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={props.array.link}
+                    >
+                        Code
+                    </a>
                 </div>
             </div>
-
         </div>
-        
-
     )
-
 }
 
 export default ProjectsElement
